e70db086005ea1e7ee582254c4b140eb957b47fa
Xabi224 <xabier.torrego@elorrieta-errekamari.com>
2022-12-13 14:15:45 +0100

    Galdetegi hecho falta conectarlo con el mapa

---
+++ app/src/main/java/com/elorrieta/didaktikapp/galdetegi/QuestionAnswer.java
@@ -0,0 +1,26 @@
+package com.elorrieta.didaktikapp.galdetegi;
+
+public class QuestionAnswer {
+
+    public static String question[] ={
+            "Nork margotu zuen “El Guernica” koadroa?",
+            "Zer ikusi daiteke koadroan?",
+            "Noiz geratatu zen gertakari hori??",
+            "Zein kolore ditu koadroak??"
+    };
+
+    public static String choices[][] = {
+            {"Velazquez","Quevedo","Picasso"},
+            {"Bonbardaketa","Eskola egun bat","Kontzertua"},
+            {"2010. urtean","1815. urtean","1937. urtean"},
+            {"Zuria eta berdea","Zuria eta beltza","Beltza eta laranja"}
+    };
+
+    public static String correctAnswers[] = {
+            "Picasso",
+            "Bonbardaketa",
+            "1937. urtean",
+            "Zuria eta beltza"
+    };
+
+}
