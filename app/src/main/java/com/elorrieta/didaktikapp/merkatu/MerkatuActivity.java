33fffc39a51503ff97144cc3004a020a28eb7634
Endika2 <113431740+Endika2@users.noreply.github.com>
2023-01-10 13:58:48 +0100

    Merkatu
    Merkatu

---
+++ app/src/main/java/com/elorrieta/didaktikapp/merkatu/MerkatuActivity.java
@@ -0,0 +1,16 @@
+package com.elorrieta.didaktikapp.merkatu;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.elorrieta.didaktikapp.R;
+
+public class MerkatuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_merkatu);
+    }
+}
---
+++ app/src/main/java/com/elorrieta/didaktikapp/merkatu/MerkatuAnswer.java
@@ -0,0 +1,18 @@
+package com.elorrieta.didaktikapp.merkatu;
+
+public class MerkatuAnswer {
+
+    public static String choices[][] = {
+            {"Erroskilak","Piperrak","Detergentea","Indabak ","Artoa","Mugikorra ","Alfonbra ",
+            "Zapatila ","Txakolina ","Yogurta","Tomateak ","Azenarioa "}
+    };
+
+    public static String correctAnswers[] = {
+            "Azenarioa",
+            "Tomateak",
+            "Artoa",
+            "Indabak",
+            "Piperrak"
+    };
+
+}
